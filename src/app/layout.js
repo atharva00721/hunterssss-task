363cecb060d5ee605c131bb5f352ba5724156290
atharva00721 <atharvaraj@gmail.com>
2024-11-06 19:34:28 +0530

    added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,8 @@ import "./globals.css";
 
 export const metadata = {
   title: "TechHunters | Tech Community & Events Platform",
-  description: "Join TechHunters - Your premier tech community hosting workshops, hackathons, speaker sessions, and innovative tech events. Connect, learn, and grow with fellow tech enthusiasts.",
+  description:
+    "Join TechHunters - Your premier tech community hosting workshops, hackathons, speaker sessions, and innovative tech events. Connect, learn, and grow with fellow tech enthusiasts.",
   keywords: [
     "tech community",
     "hackathons",
@@ -13,7 +14,7 @@ export const metadata = {
     "technology meetups",
     "coding community",
     "tech learning",
-    "developer events"
+    "developer events",
   ],
   twitter: {
     card: "summary_large_image",
@@ -30,6 +31,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-V6KDKLZBSW"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-V6KDKLZBSW');
+          `}
+        </script>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>{children}</body>
